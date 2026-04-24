bd59c4d92cf91ac00bbabd7d33cadc8d423696db
czt <changzhitao12@126.com>
2026-04-24 21:55:28 +0800

    不禁止iframe加载

--- backend/main.py
+++ backend/main.py
@@ -71,7 +71,6 @@ class SecurityHeadersMiddleware(BaseHTTPMiddleware):
     async def dispatch(self, request: StarletteRequest, call_next):
         response = await call_next(request)
         response.headers["X-Content-Type-Options"] = "nosniff"
-        response.headers["X-Frame-Options"] = "DENY"
         response.headers["X-XSS-Protection"] = "1; mode=block"
         response.headers["Referrer-Policy"] = "strict-origin-when-cross-origin"
         return response
